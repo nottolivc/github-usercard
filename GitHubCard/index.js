a96f6bf0b0c2d8af934c8a4d72e9dc6b98321817
Andrew Nottoli <nottolivc@gmail.com>
2019-08-15 18:28:54 -0700

    mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,18 +2,56 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+// axios.get(url[https://api.github.com/users/NickGallucci, config])
+// axios.get('https://api.github.com/users/NickGallucci', {
+// })
+// .then(function (response) {
+//   console.log(response);
+//   cCard.appendChild(superComp(response.data));
+// })
+// .catch(function (error) {
+//   console.log(error);
+// })
+// .then(function () {
+//   // always executed
+// });
 
+//import axios from 'axios';
+
+axios.get("https://api.github.com/users/nottolivc")
+.then(response => {
+  console.log(response);
+  cCard.appendChild(superComp(response.data));
+})
+.catch(err => {
+  console.log(err);
+})
+
+const followersArray = [];
+
+git axios.get('https://api.github.com/users/nottolivc/followers')
+.then(response =>{
+  console.log(response);
+  response.data.forEach( element =>{
+    console.log(element);
+    followersArray.push(element);
+  })
+  followersArray.forEach(element =>{
+    cCard.appendChild(superComp(element));
+  })
+})
+.catch(err => {
+  console.log(err);
+})
+console.log(followersArray);
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
-
    Skip to Step 3.
 */
-
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
-
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -23,12 +61,12 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
-
+followersArray.forEach(item =>{
+  let followerCards = superComp(item);
+  cCard.appendChild(followerCards);
+})
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
-
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -43,9 +81,43 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
-
+const cCard = document.querySelector(`.cards`)
+function superComp(obj){
+  console.log(obj)
+  const cCard = document.createElement("div");
+  img = document.createElement("img");
+  cCardInfo = document.createElement("div");
+  nameH3 = document.createElement("h3");
+  userP = document.createElement("p");
+  userLoc = document.createElement("p");
+  profile = document.createElement("p");
+  profileURL = document.createElement("a");
+  followers = document.createElement("p");
+  following = document.createElement("p");
+  bio = document.createElement("p");
+  cCard.classList.add("card");
+  nameH3.classList.add("name");
+  userP.classList.add("username");
+  cCard.appendChild(img);
+  cCard.appendChild(cCardInfo);
+  cCardInfo.appendChild(nameH3);
+  cCardInfo.appendChild(userP);
+  cCardInfo.appendChild(userLoc);
+  cCardInfo.appendChild(profile);
+  cCardInfo.appendChild(followers);
+  cCardInfo.appendChild(following);
+  cCardInfo.appendChild(bio);
+  img.src = `${obj.avatar_url}`;
+  nameH3.textContent = `${obj.login}`;
+  userP.textContent = `${obj.login}`;
+  userLoc.textContent = `${obj.location}`;
+  profileURL.textContent = `${obj.html_url}`;
+  followers.textContent = `${obj.followers}`;
+  following.textContent = ` ${obj.following}`;
+  bio.textContent = `${obj.bio}`;
+  return cCard;
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
